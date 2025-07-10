3b187880daae823a5c73ad922f6bfc37a19d13db
biraj-phuyal <birajphuyal11@gmail.com>
2025-07-10 15:09:40 +0100

    Test

--- ft_helper2.c
+++ ft_helper2.c
@@ -6,7 +6,7 @@
 /*   By: biphuyal <biphuyal@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/10 11:45:26 by biphuyal          #+#    #+#             */
-/*   Updated: 2025/07/10 15:07:19 by biphuyal         ###   ########.fr       */
+/*   Updated: 2025/07/10 15:09:30 by biphuyal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,19 +35,21 @@ int	ft_char(int c)
 
 int	ft_hexa_pointer(unsigned long long n)
 {
-	int	count = 0;
+	int	count;
 
+	count = 0;
 	if (n == 0)
 		return (write(1, "(nil)", 5));
 	count += write(1, "0x", 2);
-	count += ft_hexa_pointer_rec(n);
+	count += ft_hexa_pointer_rec(n / 16);
 	return (count);
 }
 
 int	ft_hexa_pointer_rec(unsigned long long n)
 {
-	int	count = 0;
+	int	count;
 
+	count = 0;
 	if (n >= 16)
 		count += ft_hexa_pointer_rec(n / 16);
 	count += ft_char("0123456789abcdef"[n % 16]);
